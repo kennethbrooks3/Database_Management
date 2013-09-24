715b9338e5709c6b09d41a0f8a5dbc170bc5d419
kennethbrooks3 <kenneth.brooks3@gmail.com>
2013-09-24 13:09:31 -0400

    insert statements

--- DB mgmt lab 9_24.sql
+++ DB mgmt lab 9_24.sql
@@ -4,7 +4,7 @@ create table departments(
     dateFormed date
 );
 
-
+--People
 Create Table people(
     pid int not null,
     firstname varchar(20),
@@ -12,5 +12,43 @@ Create Table people(
     hiredate date, 
     Did varchar(1), not null references Departments(Did),
    primary key(pid)
-)
+);
+
+--FY
+Create table FY(
+    year date, 
+    start date,
+    enddate date,
+   Primary key(year),
+);
     
+    
+--Insert Departments
+INSERT INTO Departments(Did, name, dateFormed)
+  VALUES('A', 'CS', '6/30/2010');
+  
+INSERT INTO Departments(Did, name, dateFormed)
+  VALUES('B', 'IT', '1/1/2013');
+  
+INSERT INTO Departments(Did, name, dateFormed)
+  VALUES('c', 'IS', '7/4/2014');
+  
+  
+--Insert People
+INSERT INTO People(Pid, Firstname, Lastname, Hiredate, Did)
+  VALUES('1', 'Kenneth', 'Brooks', '1/23/2008', 'A');
+  
+INSERT INTO People(Pid, Firstname, Lastname, Hiredate, Did)
+  VALUES('2', 'Cole', 'Roseman', '9/28/2006', 'A');
+  
+INSERT INTO People(Pid, Firstname, Lastname, Hiredate, Did)
+  VALUES('3', 'Victor', 'Firman', '4/4/2012', 'B');
+  
+INSERT INTO People(Pid, Firstname, Lastname, Hiredate, Did)
+  VALUES('4', 'Melanie', 'Savitsky', '11/11/2013', 'B');
+  
+INSERT INTO People(Pid, Firstname, Lastname, Hiredate, Did)
+  VALUES('5', 'Matt', 'Brooks', '11/11/2014', 'B');
+  
+  
+--Insert FY
